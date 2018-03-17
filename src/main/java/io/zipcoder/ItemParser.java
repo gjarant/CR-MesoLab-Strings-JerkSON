ff1a22d2514da62970c58a5623ee907b1daced62
Garrett Arant <garrettarant@MacBook-Pro-4.local>
2018-03-17 16:45:07 -0400

    Need To Format Output

--- src/main/java/io/zipcoder/ItemParser.java
+++ src/main/java/io/zipcoder/ItemParser.java
@@ -1,11 +1,12 @@
 package io.zipcoder;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ItemParser {
+
+    private Integer countExceptionsThrown = 0;
 
     private ArrayList<String> splitStringWithRegexPattern(String stringPattern, String inputString) {
         return new ArrayList<String>(Arrays.asList(inputString.split(stringPattern)));
@@ -32,13 +33,53 @@ public class ItemParser {
         return new Item(name, price, type, expiration);
     }
 
-    public String checkName(String input) throws ItemParseException{
+    public ArrayList<Item> createItemArrayList(String rawData) throws ItemParseException{
+        ArrayList<String> temp = parseRawDataIntoStringArray(rawData);
+        ArrayList<Item> itemArrayList = new ArrayList<Item>();
+
+        for (int i = 0; i <temp.size() ; i++) {
+            itemArrayList.add(parseStringIntoItem(temp.get(i)));
+        }
+        return itemArrayList;
+    }
+
+    public ArrayList<Item> filterItemArrayList(ArrayList<Item> input, String filterType){
+        ArrayList<Item> filterItemArrayList = new ArrayList<Item>();
+        for (int i = 0; i <input.size() ; i++) {
+            if(input.get(i).getName().equals(filterType))
+            filterItemArrayList.add(input.get(i));
+        }
+        return filterItemArrayList;
+    }
+
+    public Map<Double, Integer> individualItemCount(ArrayList<Item> filteredArrayList) {
+        Map<Double, Integer> priceTotals = new TreeMap<Double, Integer>(Collections.reverseOrder());
+        for (int i = 0; i < filteredArrayList.size(); i++) {
+            Double key = filteredArrayList.get(i).getPrice();
+            Integer count = priceTotals.get(key);
+            if (count == null) {
+                priceTotals.put(key, 1);
+            } else {
+                priceTotals.put(key, count + 1);
+            }
+        }
+        return priceTotals;
+    }
+
+    public String checkName(String input) throws ItemParseException {
+        String newInput = fixCookie(input);
         Pattern patternName = Pattern.compile("([Nn]..[Ee]:)(\\w+)");
-        Matcher matcherName= patternName.matcher(input);
+        Matcher matcherName= patternName.matcher(newInput);
 
         if (matcherName.find())
             return matcherName.group(2).toLowerCase();
         else throw new ItemParseException();
+    }
+
+    public String fixCookie(String input){
+        Pattern patternCookies = Pattern.compile("([Cc][0Oo][0Oo][Kk][Ii][Ee][Ss])");
+        Matcher matcherCookie= patternCookies.matcher(input);
+        return matcherCookie.replaceAll("cookies");
     }
 
     public String checkPrice(String input) throws ItemParseException{
@@ -67,4 +108,45 @@ public class ItemParser {
             return matcherExpiration.group(2);
         else throw new ItemParseException();
     }
+
+    public Integer getExceptionsThrown(String rawData)  {
+
+        ArrayList<String> parsedRawData = parseRawDataIntoStringArray(rawData);
+        for (int i = 0; i < parsedRawData.size(); i++)
+            try {
+                parseStringIntoItem(parsedRawData.get(i));
+            } catch (ItemParseException e){
+            }
+        return this.countExceptionsThrown;
+    }
 }
+
+
+//    public String checkName(String input) throws ItemParseException{
+//        Pattern patternName = Pattern.compile("([Nn]..[Ee]:)(\\w+)");
+//        Matcher matcherName= patternName.matcher(input);
+//
+//        if (matcherName.find())
+//            return matcherName.group(2).toLowerCase();
+//        else throw new ItemParseException();
+//    }
+
+//    Pattern[] patternName = new Pattern[4];
+//        patternName[0] = Pattern.compile("([Nn]..[Ee]:)([Mm]..[Kk])");
+//                patternName[1] = Pattern.compile("([Nn]..[Ee]:)([Bb]...[Dd])");
+//                patternName[2] = Pattern.compile("([Nn]..[Ee]:)([Cc].....[Ss])");
+//                patternName[3] = Pattern.compile("([Nn]..[Ee]:)([Aa]....[Ss])");
+//
+//                for (int i = 0; i < patternName.length; i++) {
+//        Matcher nameMatcher = patternName[i].matcher(input);
+//        if (nameMatcher.find()) {
+//        return "milk";
+//        } else if (nameMatcher.find()) {
+//        return "bread";
+//        } else if (nameMatcher.find()) {
+//        return "cookies";
+//        } else if (nameMatcher.find()) {
+//        return "apples";
+//        }
+//        }
+//        throw new ItemParseException();
--- src/test/java/io/zipcoder/ItemParserTest.java
+++ src/test/java/io/zipcoder/ItemParserTest.java
@@ -19,6 +19,13 @@ public class ItemParserTest {
     private String rawMultipleItems = "naMe:Milk;price:3.23;type:Food;expiration:1/25/2016##"
                                       +"naME:BreaD;price:1.23;type:Food;expiration:1/02/2016##"
                                       +"NAMe:BrEAD;price:1.23;type:Food;expiration:2/25/2016##";
+    private String rawMultipleItems2 = "naMe:Milk;price:3.23;type:Food;expiration:1/25/2016##"
+                                    +"naME:BreaD;price:1.23;type:Food;expiration:1/02/2016##"
+                                    +"NAMe:BrEAD;price:3.23;type:Food;expiration:2/25/2016##"
+                                    +"NAMe:BrEAD;price:3.23;type:Food;expiration:2/25/2016##"
+                                    +"NAMe:BrEAD;price:1.23;type:Food;expiration:2/25/2016##"
+                                    +"NAMe:BrEAD;price:2.23;type:Food;expiration:2/25/2016##";
+
     private ItemParser itemParser;
 
     @Before
@@ -57,6 +64,81 @@ public class ItemParserTest {
     public void findKeyValuePairsInRawItemDataTestIrregular(){
         Integer expected = 4;
         Integer actual = itemParser.findKeyValuePairsInRawItemData(rawSingleItemIrregularSeperatorSample).size();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void createItemArrayListTest() throws ItemParseException {
+        String expected ="name:milk price:3.23 type:food expiration:1/25/2016";
+        String actual  = itemParser.createItemArrayList(rawMultipleItems).get(0).toString();
+        System.out.println(itemParser.createItemArrayList(rawMultipleItems));
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void filterItemArrayListTest() throws ItemParseException {
+        ArrayList<Item> filterItemArrayListTester = itemParser.createItemArrayList(rawMultipleItems);
+        String expected ="[name:bread price:1.23 type:food expiration:1/02/2016, name:bread price:1.23 type:food expiration:2/25/2016]";
+        String actual  = itemParser.filterItemArrayList(filterItemArrayListTester,"bread").toString();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void individualItemCountTest1() throws ItemParseException {
+        ArrayList<Item> individualItemCountTester = itemParser.filterItemArrayList(itemParser.createItemArrayList(rawMultipleItems),"bread");
+        String expected ="{1.23=2}";
+        String actual  = itemParser.individualItemCount(individualItemCountTester).toString();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void individualItemCountTest2() throws ItemParseException {
+        ArrayList<Item> individualItemCountTester = itemParser.filterItemArrayList(itemParser.createItemArrayList(rawMultipleItems2),"bread");
+        String expected ="{3.23=2, 2.23=1, 1.23=2}";
+        String actual  = itemParser.individualItemCount(individualItemCountTester).toString();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void checkNameTest1() throws ItemParseException{
+        String rawItemTest = "naMe:c00kies;price:3.23;type:Food;expiration:1/25/2016##";
+        String expected = "cookies";
+        String  actual = itemParser.checkName(rawItemTest);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void checkNameTest2() throws ItemParseException{
+        String rawItemTest = "naMe:mIlk;price:3.23;type:Food;expiration:1/25/2016##";
+        String expected = "milk";
+        String  actual = itemParser.checkName(rawItemTest);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void checkPriceTest() throws ItemParseException{
+        String rawItemTest = "naMe:Milk;price:3.23;type:Food;expiration:1/25/2016##";
+        String expected = "3.23";
+        String  actual = itemParser.checkPrice(rawItemTest);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void checkTypeTest() throws ItemParseException{
+        String rawItemTest = "naMe:Milk;price:3.23;type:Food;expiration:1/25/2016##";
+        String expected = "food";
+        String  actual = itemParser.checkType(rawItemTest);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void checkExpirationTest() throws ItemParseException{
+        String rawItemTest = "naMe:Milk;price:3.23;type:Food;expiration:1/25/2016##";
+        String expected = "1/25/2016";
+        String  actual = itemParser.checkExpiration(rawItemTest);
         assertEquals(expected, actual);
     }
 }
