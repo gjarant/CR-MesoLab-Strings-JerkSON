42bcec25661abfdbb92bb47b85fc06eaee8aefae
Garrett Arant <garrettarant@MacBook-Pro-4.fios-router.home>
2018-03-16 23:22:24 -0400

    Work in Progress

--- src/main/java/io/zipcoder/ItemParser.java
+++ src/main/java/io/zipcoder/ItemParser.java
@@ -2,30 +2,69 @@ package io.zipcoder;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ItemParser {
 
-
-    public ArrayList<String> parseRawDataIntoStringArray(String rawData){
-        String stringPattern = "##";
-        ArrayList<String> response = splitStringWithRegexPattern(stringPattern , rawData);
-        return response;
-    }
-
-    public Item parseStringIntoItem(String rawItem) throws ItemParseException{
-        return null;
-    }
-
-    public ArrayList<String> findKeyValuePairsInRawItemData(String rawItem){
-        String stringPattern = "[;|^]";
-        ArrayList<String> response = splitStringWithRegexPattern(stringPattern , rawItem);
-        return response;
-    }
-
-    private ArrayList<String> splitStringWithRegexPattern(String stringPattern, String inputString){
+    private ArrayList<String> splitStringWithRegexPattern(String stringPattern, String inputString) {
         return new ArrayList<String>(Arrays.asList(inputString.split(stringPattern)));
     }
 
+    public ArrayList<String> parseRawDataIntoStringArray(String rawData) {
+        String stringPattern = "##";
+        ArrayList<String> response = splitStringWithRegexPattern(stringPattern, rawData);
+        return response;
+    }
 
+    public ArrayList<String> findKeyValuePairsInRawItemData(String rawItem) {
+        String stringPattern = "[@|^|*|%|!|;]";
+        ArrayList<String> response = splitStringWithRegexPattern(stringPattern, rawItem);
+        return response;
+    }
 
+    public Item parseStringIntoItem(String rawItem) throws ItemParseException {
+        String name = checkName(rawItem);
+        Double price = Double.valueOf(checkPrice(rawItem));
+        String type = checkType(rawItem);
+        String expiration = checkExpiration(rawItem);
+
+        return new Item(name, price, type, expiration);
+    }
+
+    public String checkName(String input) throws ItemParseException{
+        Pattern patternName = Pattern.compile("([Nn]..[Ee]:)(\\w+)");
+        Matcher matcherName= patternName.matcher(input);
+
+        if (matcherName.find())
+            return matcherName.group(2).toLowerCase();
+        else throw new ItemParseException();
+    }
+
+    public String checkPrice(String input) throws ItemParseException{
+        Pattern patternPrice = Pattern.compile("([Pp]...[Ee]:)(\\d\\.\\d{2})");
+        Matcher matcherPrice= patternPrice.matcher(input);
+
+        if (matcherPrice.find())
+            return matcherPrice.group(2);
+        else throw new ItemParseException();
+    }
+
+    public String checkType(String input) throws ItemParseException{
+        Pattern patternType = Pattern.compile("([Tt]..[Ee]:)(\\w+)");
+        Matcher matcherType = patternType.matcher(input);
+
+        if (matcherType.find())
+            return matcherType.group(2).toLowerCase();
+        else throw new ItemParseException();
+    }
+
+    public String checkExpiration(String input) throws ItemParseException{
+        Pattern patternExpiration = Pattern.compile("([Ee]........[Nn]:)(\\d\\/\\d{2}\\/\\d{4})");
+        Matcher matcherExpiration = patternExpiration.matcher(input);
+
+        if (matcherExpiration.find())
+            return matcherExpiration.group(2);
+        else throw new ItemParseException();
+    }
 }
